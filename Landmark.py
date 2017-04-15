c053bf90cc03b1f3944b1bddef56b43ed162f8e0
tannavee-watts <tanvi.watts94@gmail.com>
2017-04-14 23:48:12 -0400

    Update Landmark.py

--- Landmark.py
+++ Landmark.py
@@ -5,16 +5,16 @@ from pyspark import SparkContext
 import csv
 
 # Cleaning Landmark (Column 17)
-
 import re
-
 from datetime import datetime
 
 tests = [
     # (Type, Test)
     (int, int),
     (float, float),
-    (str, str)
+    (str, str),
+    (datetime, lambda value: datetime.strptime(value, "%Y/%m/%d"))
+
 ]
 
 
@@ -26,25 +26,33 @@ def getValid(Landmark):
 
 
 def getDataType(x):
-    # label = "invalid"
-    myVal = getValid(x)
-    if myVal:
-        typ = "str"
-    else:
-        for typ, test in tests:
-            try:
-                test(x)
-            except ValueError:
-                continue
-    # return(myVal)
-    if not myVal:
-        label = "invalid"
-    else:
-        label = "valid"
-    typ = str(typ).replace('<class','').strip('>').strip(' ').strip('\'')
-    return str(x + ', ' + str(typ) + ', ' + 'Landmark, ' + label)
+    label = "invalid"
 
+    for typ, test in tests:
+        try:
+            test(x)
+            if typ == str:
+                pattern = re.compile("^(?:[A-Z0-9 \/])+$")
+                if pattern.match(x):
+                    label = "valid"
+                    break
+                else:
+                    if x == '':
+                        label = "N/A"
+                    else:
+                        label = "invalid"
+                    break;
+            if typ == int:
+                break
+            if typ == float:
+                break
+            else:
+                break
 
+        except ValueError:
+            continue
+
+    return str(str(x)+', '+str(typ).replace('<class', '').strip('>')+', '+'Landmark Name, '+label)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
