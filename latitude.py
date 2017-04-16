081d316a91ccbfdec31a7e5420f6d2117433a608
Anshal N Savla <ans605@login-2-1.local>
2017-04-16 16:59:24 -0400

    commit 4

---
+++ latitude.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import sys
+from pyspark import SparkContext
+import csv
+import re
+from datetime import datetime
+
+tests = [
+    # (Type, Test)
+    (int, int),
+    (float, float),
+    (str, str),
+    (datetime, lambda value: datetime.strptime(value, "%Y/%m/%d"))
+
+]
+
+
+def getDataType(x):
+    label = "invalid"
+
+    for typ, test in tests:
+        try:
+            test(x)
+            if typ == float:
+                pattern = re.compile('^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$')
+                if pattern.match(x):
+                    label = "valid"
+                break
+
+            if typ == int:
+                break
+            if typ == str and x == '':
+                label="N/A"
+                break
+            else:
+                break
+
+        except ValueError:
+            continue
+
+    return str(str(x)+', '+str(typ).replace('<class', '').strip('>')+', '+'Latitude, '+label)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: latitude.py <file>")
+        exit(-1)
+    sc = SparkContext()
+    csvfile = sc.textFile("new_311.csv")
+    incidentAddress = csvfile.mapPartitions(lambda x: csv.reader(x)).map(lambda x: x[49])
+    base_type=incidentAddress.map(lambda x: getDataType(x))
+    base_type.saveAsTextFile("latitude.txt")
+    sc.stop()
+
