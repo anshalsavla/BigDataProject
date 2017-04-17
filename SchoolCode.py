4228630fb0330fc469c38b363da22e0c21404098
Tannavee Watts <tw1555@login-2-1.local>
2017-04-17 12:05:43 -0400

    SchoolCode

--- SchoolCode.py
+++ SchoolCode.py
@@ -55,8 +55,9 @@ if __name__ == "__main__":
         print("Usage: SchoolCode.py <file>")
         exit(-1)
     sc = SparkContext()
-    csvfile = sc.textFile("new_311.csv")
+    csvfile = sc.textFile(sys.argv[1],1)
     school = csvfile.mapPartitions(lambda x: csv.reader(x)).map(lambda x: x[31])
     base_type=school.map(lambda x: getDataType(x))
     base_type.saveAsTextFile("SchoolCode.txt")
 sc.stop()
+
